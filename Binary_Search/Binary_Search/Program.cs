b74d1b3b2a30abb96a897b96237f7b3170d0481b
guillermodev18 <guillewdc@gmail.com>
2022-03-15 22:06:58 -0400

    BinariSearch incomplete Commit

---
+++ Binary_Search/Binary_Search/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Program{
+
+    public static void Main(string[] args){
+
+        // list
+        int[] list_1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15 };
+
+    }
+
+    public static int BinarySearch(int[] list, int number){
+
+        int low = 0; 
+        int high = list.Length;
+        int mid = list.Length / 2;
+
+        do
+        {
+
+            if(list[mid] == number){
+
+                return mid;
+
+            }else if(number > list[mid]){
+
+                low = mid + 1;
+                //mid = ;
+
+            }else if(number < list[mid]){
+
+               // high = ;
+            }
+
+        } while (list[mid] != number);
+
+
+
+
+    }
+}
