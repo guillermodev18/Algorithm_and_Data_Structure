87bd78f946c429685d4c5f85f5daa6a5c3d4146f
guillermodev18 <guillewdc@gmail.com>
2022-03-28 21:36:34 -0400

    Binari_search continuen fixing

--- Binary_Search/Binary_Search/Program.cs
+++ Binary_Search/Binary_Search/Program.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using System.Collections;
+using System.ComponentModel;
+using System;
 
 public class Program{
 
+    // Recursive way
     // Returns index of number if it is present in
     // arr[low..high], else return -1
-    public static int BinarySearch(int[] list, int low, int high, int number){
+    public static int RecursiveBinarySearch(int[] list, int low, int high, int number){
 
         if(high >= low){
 
@@ -12,19 +15,19 @@ public class Program{
 
             // If the element is present at the
             // middle itself
-            if(mid == number){
+            if(list[mid] == number){
                 return mid;
             }
 
             // If element is smaller than mid, then
             // it can only be present in left subarray
             if(list[mid] > number){
-                return BinarySearch(list, low, mid - 1, number);
+                return RecursiveBinarySearch(list, low, mid - 1, number);
             }
 
             // Else the element can only be present
             // in right subarray
-            return BinarySearch(list, mid + 1, high, number);
+            return RecursiveBinarySearch(list, mid + 1, high, number);
         }
 
         // We reach here when element is not present
@@ -32,14 +35,40 @@ public class Program{
         return -1;
     }
 
+
+    // Iterative Binary Search
+    // Returns index of number if it is present in arr[],
+    // else return -1
+    public static int IterativeBinarySearch(int[] array, int number){
+
+        int low = 0;
+        int high = array.Length - 1;
+
+        while(low <= high){
+
+            int mid = low + (high - low)/2;
+
+            // Check if x is present at mid
+            if(array[mid] == number){
+                return mid;
+            }
+
+            // If number greater , ignore left half
+        }
+
+
+    }
+
+
     public static void Main(string[] args){
 
 
         int[] array = { 2, 3, 4, 10, 40 };
         int n = array.Length;
-        int x = 10;
- 
-        int result = BinarySearch(array, 0, n - 1, x);
+        int x = 1;
+
+        // 
+        int result = RecursiveBinarySearch(array, 0, n - 1, x);
  
         if (result == -1)
             Console.WriteLine("Element not present");
